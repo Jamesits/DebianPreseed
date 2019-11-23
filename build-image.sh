10616357cc46c875b7dea3e7b4b581945da1543d
James Swineson <github@public.swineson.me>
2019-11-23 07:59:10 -0800

    basic preseed file test

--- build-image.sh
+++ build-image.sh
@@ -54,7 +54,7 @@ pack_img() {
 	pushd "$BUILD_DIR/isoroot"
 	md5sum `find -follow -type f` > md5sum.txt
 	popd
-	xorriso -as mkisofs -r -checksum_algorithm_iso md5,sha1,sha256,sha512 -V "${DEST_IMG_NAME}"  -o "$BUILD_DIR/${DEST_IMG_BASENAME}" -J -joliet-long -cache-inodes -isohybrid-mbr isohdpfx.bin -b isolinux/isolinux.bin -c isolinux/boot.cat -boot-load-size 4 -boot-info-table -no-emul-boot -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus isoroot
+	xorriso -as mkisofs -r -checksum_algorithm_iso md5,sha1,sha256,sha512 -V "Debian preseeded"  -o "$BUILD_DIR/${DEST_IMG_BASENAME}" -J -joliet-long -cache-inodes -isohybrid-mbr isohdpfx.bin -b isolinux/isolinux.bin -c isolinux/boot.cat -boot-load-size 4 -boot-info-table -no-emul-boot -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus isoroot
 }
 
 load_config
